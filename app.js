e4a970c2da26c1779f1f3895511b377db1a26d9e
MasterKelwin <mkelwin@outlook.com>
2022-05-22 13:26:41 -0300

    adicionando histórico na calculadora

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let resultadoCalculo;
+const historico = document.querySelector('.historico');
+
 function inserir(number) {
     var resultadoInserir = document.getElementById('resultado');
     resultadoInserir.innerHTML += number;
@@ -7,16 +10,16 @@ function limpar() {
     resultado.innerHTML = "";
 }
 
-function calcular()
-{
+function calcular() {
     var resultadoCalcular = document.getElementById('resultado').innerHTML;
-    if(resultadoCalcular)
-    {
-        document.getElementById('resultado').innerHTML = eval(resultadoCalcular);
+    if(resultadoCalcular) {
+        const resultadoNaTela = document.getElementById('resultado');
+        resultadoCalculo = eval(resultadoCalcular).toFixed(2);
+        resultadoNaTela.innerHTML = resultadoCalculo;
+        geraHistorico();
     }
-    else
-    {
-        document.getElementById('resultado').innerHTML = "Nada..."
+    else {
+        document.getElementById('resultado').innerHTML = "Erro";
     }
 }
 
@@ -25,41 +28,17 @@ function apagar() {
     document.getElementById('resultado').innerHTML = resultadoApagar.substring(0, resultadoApagar.length -1);
 }
 
-const geraHTML = () => {
-    const containerExecutandoApp = document.createElement("div");
-    containerExecutandoApp.classList.add("container-executando-app");
-
-    const containerSenha = document.createElement("div");
-    containerSenha.classList.add("container-senha");
-
-    const containerImagens = document.createElement("div");
-    containerImagens.classList.add("container-imagens");
-
-    const titulo = document.createElement("h1");
-    titulo.innerText = "SENHA";
-    titulo.classList.add("senha__titulo");
-
-    const senhaReal = document.createElement("p");
-    senhaReal.classList.add("senha__paragrafo");
-    senhaReal.innerText = input.value;
-
-    const imagem = document.createElement("img");
-    imagem.classList.add("imagem");
-    imagem.src = "assets/imgs/01.png";
-
-    senhaDiv.appendChild(containerExecutandoApp);
-    containerExecutandoApp.appendChild(containerImagens);
-    containerExecutandoApp.appendChild(containerSenha);
-    containerImagens.appendChild(imagem);
-    containerSenha.appendChild(titulo);
-    containerSenha.appendChild(senhaReal);
+function geraHistorico() {
+    const resultadoHistorico = document.createElement("p");
+    resultadoHistorico.innerHTML = resultadoCalculo;
+    historico.appendChild(resultadoHistorico);
 }
-
 
 document.addEventListener('keypress', function (e) {
     if (e.which == 13) { //enter
-        calcula();
+        calcular();
     } else {
         return;
     }
 });
+
