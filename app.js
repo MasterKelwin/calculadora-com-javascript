2d770c91b435a00d60eae88ef73c9f42f7812a14
MasterKelwin <mkelwin@outlook.com>
2022-05-24 13:51:51 -0300

    refatoração e nova feature

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+//'use strict';
+
 let resultadoCalculo;
 const resultadoNaTela = document.getElementById('resultado');
+resultadoNaTela.focus();
 const historico = document.querySelector('.historico');
 
 function inserir(number) {
@@ -73,9 +76,33 @@ function gerarHistorico() {
     historico.appendChild(resultadoHistorico);
 }
 
-document.addEventListener('keypress', function (e) {
-    if (e.which == 13) { //enter
-        calcular();
-    } 
-});
+const mapaTeclado = {
+    1: 'tecla1',
+    2: 'tecla2',
+    3: 'tecla3',
+    4: 'tecla4',
+    5: 'tecla5',
+    6: 'tecla6',
+    7: 'tecla7',
+    8: 'tecla8',
+    9: 'tecla9',
+    0: 'tecla0',
+    '/': 'tecla/',
+    '*': 'tecla*',
+    '-': 'tecla-',
+    '+': 'tecla+',
+    '=': 'tecla=',
+    Enter: 'tecla=',
+    Backspace: 'teclaDelete',
+    c: 'teclaC',
+    Escape: 'teclaDelete',
+    ',': 'tecla.',
+};
 
+function mapearTeclado(evento) {
+    const tecla = evento.key;
+    const teclaPermitida = () => Object.keys(mapaTeclado).indexOf(tecla) !== -1;
+    if (teclaPermitida()) document.getElementById(mapaTeclado[tecla]).click();
+}
+
+document.addEventListener('keydown', mapearTeclado);
